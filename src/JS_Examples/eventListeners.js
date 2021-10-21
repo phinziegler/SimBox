7ec07ded14b25d7c01c1d48423752b3c8c909b42
Phineas Ziegler <phinziegler@gmail.com>
2021-10-21 13:43:01 -0400

    more styles, updated example js added fontawesome fonts and included inputs for options instead of just text

--- src/JS_Examples/eventListeners.js
+++ src/JS_Examples/eventListeners.js
@@ -11,5 +11,10 @@ myElement.addEventListener("click",(e) => {
 In this case, myElment corresponds to the html element with id 'element1'...
 Thus we are adding the ability to CLICK on element1. When element 1 is CLICKED, myFunction will be called.
 There are MANY other events that can be listened for... keydown, keyup, onMenu, etc.
-
 */
+
+function myFunction() {
+    console.log("click event!");
+}
+
+// This is how functions are defined when they are defined OUTSIDE of a class...
