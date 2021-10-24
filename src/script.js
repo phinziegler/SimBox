3326fe50d24abcb1e96f80a0e3a1df1727953095
Phineas Ziegler <phinziegler@gmail.com>
2021-10-24 14:59:19 -0400

    fixed imports

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
-import InputHandler from "./Tools and Input/inputHandler.js";
-import Options from "./Tools and Input/options.js";
-import Tools from "./Tools and Input/tool.js";
+import InputHandler from "./tools-and-input/inputHandler.js";
+import Options from "./tools-and-input/options.js";
+import Tools from "./tools-and-input/tool.js";
 
 
 // USE THIS TO INSTANTIATE CLASSES
