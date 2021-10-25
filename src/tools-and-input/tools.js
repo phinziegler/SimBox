a35c6f8212f629bf8d6902bcbdd6735e30960e31
benmucha <43706062+benmucha@users.noreply.github.com>
2021-10-24 21:10:35 -0400

    Add active tool visual indicator
    Add visual indicator for the active tool in the UI such that the currently active tool button has a distinct color scheme that sets it apart from the other tool options.

--- src/tools-and-input/tools.js
+++ src/tools-and-input/tools.js
@@ -9,22 +9,42 @@ export const tools = {
     GRAB: "grab",
 }
 
+/**
+ * The name of the active tool class.
+ */
+const activeToolClass = "activeTool";
+
+/**
+ * The active tool by default.
+ */
+const defaultActiveTool = tools.RECTANGLE;
+
 export default class ToolsHandler {
     constructor() {
+        /**
+         * The element that contains the tools elements.
+         * @type {Element}
+         */
+        this.toolsContainerElement = document.querySelector("#menuPanel");
         /**
          * List of all tool IDs.
          * @type {Element[]}
          */
-        this.list = Array.from(document.querySelectorAll(".menuToolButton"));
+        this.list = Array.from(this.toolsContainerElement.querySelectorAll(".menuToolButton"));
+        this.buildToolsMenuUI();
+    }
 
+    /**
+     * Builds the UI for the tools menu and respective buttons.
+     */
+    buildToolsMenuUI(){
         // ADD EVENT LISTENER TO ALL TOOLS -- when a tool button is clicked it sets that tool to the current active tool
         for(let i = 0; i < this.list.length; i++) {
             this.list[i].addEventListener("click", () => {
                 this.activeTool = this.list[i].id;
             });
         }
-
-        this.activeTool = tools.RECTANGLE; // Active tool is rectangle by default.
+        this.activeTool = defaultActiveTool; // Set the default active tool once the UI is constructed.
     }
 
     /**
@@ -34,8 +54,18 @@ export default class ToolsHandler {
     get activeTool() {
         return this._activeTool;
     }
+
     set activeTool(tool) {
         this._activeTool = tool;
-        console.log(this.activeTool + " active");
+
+        // Remove all current visual indicators of the currently active tool.
+        const currentActiveTools = this.toolsContainerElement.querySelectorAll(`.${activeToolClass}`);
+        for (const activeTool of currentActiveTools) {
+            activeTool.classList.remove(activeToolClass);
+        }
+
+        this.toolsContainerElement.querySelector(`#${tool}`).classList.add(activeToolClass); // Set the new visual indicator for the currently active tool.
+        
+        //console.log(this.activeTool + " active");
     }
 }
