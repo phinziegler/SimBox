7e4dd8fdd78ffd5b1b5d41964ed647592bff1d4a
benmucha <43706062+benmucha@users.noreply.github.com>
2021-10-25 14:38:32 -0400

    Fix simulator test load html
    Fix test to actually load the index html file in the test suite so that UI can potentially be tested.

--- src/simulator.test.js
+++ src/simulator.test.js
@@ -1,17 +1,15 @@
 import Simulator from "./simulator.js";
 
 global.PIXI = jest.fn();
-global.PIXI.Renderer = jest.fn().mockImplementation(() => {
-  return {
-    
-  };
+global.PIXI.Renderer = jest.fn(() => {
+  return {};
 });
-global.PIXI.Container = jest.fn().mockImplementation(() => {
+global.PIXI.Container = jest.fn(() => {
   return {
     addChild: jest.fn()
   };
 });
-global.PIXI.Graphics = jest.fn().mockImplementation(() => {
+global.PIXI.Graphics = jest.fn(() => {
   return {
     beginFill: jest.fn(),
     lineStyle: jest.fn(),
@@ -27,16 +25,19 @@ global.planck = require('planck-js');
 global.window = global; // Mock 'window' as the global namespace as it is in a typical JS environment.
 global.window.requestAnimationFrame = jest.fn();
 
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const document = new JSDOM("<!DOCTYPE html>").window.document;
-
+const { JSDOM } = require("jsdom");
 
 
 describe("Simulator", () => {
+  beforeAll(() => {
+    return JSDOM.fromFile("./index.html").then((dom) => { // Importantly, the scripts inside the html should not be loaded by JSDOM by default.
+      global.document = dom.window.document;
+    });
+  });
   test("should set canvas through constructor", () => {
-    const mockCanvas = document.createElement("canvas");
-    const simulator = new Simulator(mockCanvas);
-    expect(simulator.simulationCanvas).toEqual(mockCanvas);
+    const canvas = document.getElementById("canvas");
+    console.log(document.getElementsByTagName("script"))
+    const simulator = new Simulator(canvas);
+    expect(simulator.simulationCanvas).toEqual(canvas);
   });
 });
